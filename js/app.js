98f27f23a5baf0dc034b0c75a5d2c60bb677435a
MarySejk <mariekohlova@gmail.com>
2025-03-21 09:27:12 +0100

    vytvoreni slozek

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+    return (
+        <>
+        </>
+    );
+}
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
